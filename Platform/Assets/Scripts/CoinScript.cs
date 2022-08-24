59daaaff9d2c182079704b4fa0f4555095621e4f
Paulo <paulofs10.sea@gmail.com>
2022-08-23 23:28:52 -0300

    add SFX to coins

---
+++ Platform/Assets/Scripts/CoinScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class CoinScript : MonoBehaviour
+{
+
+    public Object ScriptTarget;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            ScriptTarget = GameObject.Find("ScriptObject");
+            this.transform.position = new Vector3(0, -30, 0);
+            this.GetComponent<AudioSource>().Play();
+            ScriptTarget.GetComponent<ScriptObjectScript>().Coins += 1;
+            Destroy(this.gameObject, 0.5f);
+        }
+    }
+
+}
